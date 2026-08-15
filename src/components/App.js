231287ab5ea85698ae681acb3328737cef65abcd
ARAVINTH0022 <ARAVINTH0022@users.noreply.github.com>
2026-08-15 23:08:52 +0530

    Implement React Router Home and About solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Navigation from './Navigation';
+import '../styles/App.css';
 
 const App = () => {
   return (
-    <div>
+    <Router>
+      <div id="main">
         {/* Do not remove the main div */}
-    </div>
-  )
-}
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
